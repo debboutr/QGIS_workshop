ee7b2041cfab2ac4aaff5333fc2214a9cee0d345
Rick Debbout <debboutr@gmail.com>
2017-03-20 17:01:34 -0700

    Create workshop.py

---
+++ workshop.py
@@ -0,0 +1,36 @@
+from qgis.core import *
+import processing
+import numpy as np
+
+def findUpstreamNpy(zone, com, npy_file):
+    '''
+    __author__ =  "Rick Debbout <debbout.rick@epa.gov>"
+    Finds upstream array of COMIDs for any given catchment COMID
+
+    Arguments
+    ---------
+    zone                  : string of an NHDPlusV2 VPU zone, i.e. 10L, 16, 17
+    com                   : COMID of NHD Catchment, integer
+    numpy_dir             : directory where .npz file is stored
+    '''
+    npzfile = np.load(outfile)
+    comids = npzfile['comids']
+    lengths= npzfile['lengths']
+    upStream = npzfile['upStream']
+    itemindex = int(np.where(comids == com)[0])
+    n = lengths[:itemindex].sum()
+    arrlen = lengths[itemindex]
+    return upStream[n:n+arrlen]
+    
+f = 'D:/NHDPlusV21/StreamCat_npy/children'
+zone = '17'
+com = 23763517 # COMID where the Calapooia meets the Willamette
+wsIds = findUpstreamNpy(zone, com, f)
+
+print wsIds
+
+layer = processing.getObject('Catchments')
+layer.featureCount()
+query = "\"FEATUREID\" in %s" % str(tuple(wsIds))
+selection = layer.getFeatures(QgsFeatureRequest().setFilterExpression(query))
+layer.setSelectedFeatures([k.id() for k in selection])
