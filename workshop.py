f2b6966da567c3730c03092319004cd45131ad72
Rick Debbout <debboutr@gmail.com>
2017-04-04 19:28:01 -0700

    Update workshop.py

--- workshop.py
+++ workshop.py
@@ -2,18 +2,17 @@ from qgis.core import *
 import processing
 import numpy as np
 
-def findUpstreamNpy(zone, com, npy_file):
+def findUpstreamNpy(com, npy_file):
     '''
     __author__ =  "Rick Debbout <debbout.rick@epa.gov>"
     Finds upstream array of COMIDs for any given catchment COMID
-
     Arguments
     ---------
     zone                  : string of an NHDPlusV2 VPU zone, i.e. 10L, 16, 17
     com                   : COMID of NHD Catchment, integer
     numpy_dir             : directory where .npz file is stored
     '''
-    npzfile = np.load(outfile)
+    npzfile = np.load(npy_file)
     comids = npzfile['comids']
     lengths= npzfile['lengths']
     upStream = npzfile['upStream']
@@ -22,11 +21,10 @@ def findUpstreamNpy(zone, com, npy_file):
     arrlen = lengths[itemindex]
     return upStream[n:n+arrlen]
     
-f = 'D:/NHDPlusV21/StreamCat_npy/children'
-zone = '17'
+f = '/home/rick/Dropbox/QGIS_workshop/upstream_arrays.npz'
 com = 23763517 # COMID where the Calapooia meets the Willamette
-wsIds = findUpstreamNpy(zone, com, f)
-
+wsIds = findUpstreamNpy(com, f)
+len(wsIds)
 print wsIds
 
 layer = processing.getObject('Catchments')
